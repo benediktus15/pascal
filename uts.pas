cf922cabcd0f736eaecde7316784f22447eced82
ben <benedyctoes@gmail.com>
2019-08-07 18:35:59 +0700

    Update uts.pas

--- uts.pas
+++ uts.pas
@@ -1,11 +1,23 @@
-program UTS_DASAR_PEMROGRAMAN;
+program UTS_DASAR_PEMROGRAMAN_BENEDIKTUS_ZEBUA;
 
 uses crt;
 
 var
-        NA, NT, NQ, NUTS, NUAS, NIM: integer;
-        NAMA: string[50];
-        NTC, NQC, NUTSC, NUASC: real;
+        NT, NQ, NUTS, NUAS, NIM: integer;
+        keterangan, NAMA: string[50];
+        NA, NTC, NQC, NUTSC, NUASC: real;
+        N: char;
+        
+        
+procedure HASIL(N, keterangan);
+begin
+    clrscr;
+    writeln('NIM            : ', NIM);
+    writeln('NAMA           : ', NAMA);
+    writeln('NILAI AKHIR    : ', NA);
+    writeln('GRADE          : ', N);
+    writeln('KETERANGAN     : ', keterangan);
+end;
 
 begin
         clrscr;
@@ -20,25 +32,43 @@ begin
 
         {PROSES}
 
-        {NTC:=10/100*NT;
-        NQC:=20/100*NQ;
-        NUTSC:=30/100*NUTS;
-        NUASC:=40/100*NUAS;  }
+        NTC:=0.10*NT;
+        NQC:=0.20*NQ;
+        NUTSC:=0.30*NUTS;
+        NUASC:=0.40*NUAS;
 
-        {NA:=NTC+NQC+NUTSC+NUASC;}
+        NA:=NTC+NQC+NUTSC+NUASC;
 
-        NA:=85;
-
-        writeln(NA:0);
+        writeln(NA:2:0);
 
         if ( NA >= 80 ) then
-                writeln('GRADE A')
+            begin
+                {writeln('GRADE A')}
+                N:='A';
+                HASIL(NA, "MEMUASKAN");
+            end
         else if (NA >= 75) then
-                writeln('GRADE B')
+            begin
+                {writeln('GRADE A')}
+                N:='B';
+                HASIL(NA, "MEMUASKAN");
+            end
         else if (NA >= 60) then
-                writeln('GRADE C')
+            begin
+                {writeln('GRADE A')}
+                N:='C';
+                HASIL(NA, "MEMUASKAN");
+            end
         else if (NA >= 49 ) then
-                writeln('GRADE D')
+            begin
+                {writeln('GRADE A')}
+                N:='D';
+                HASIL(NA, "MEMUASKAN");
+            end
         else
-                writeln('GRADE E');
+            begin
+                {writeln('GRADE A')}
+                N:='E';
+                HASIL(NA, "MEMUASKAN");
+            end;
 end.
